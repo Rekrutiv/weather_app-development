157c9420aa3d289463f661236c610494402013bc
Rekrutiv <42693130+Rekrutiv@users.noreply.github.com>
2022-03-29 20:54:22 +0300

    Delete boxes.dart

--- lib/boxes.dart
+++
@@ -1,7 +0,0 @@
-import 'package:hive/hive.dart';
-import 'models/db/weather_model_db.dart';
-
-class Boxes {
-  static Box<WeatherModelDB> getTransactions() =>
-      Hive.box<WeatherModelDB>('transactions');
-}
